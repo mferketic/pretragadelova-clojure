5f7cfe99c0d1defdd23591e08f327bff25a39229
mferketic <marioskola1990@gmail.com>
2023-08-09 13:00:36 +0200

    Partially adding part search for major service

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -32,7 +32,7 @@ VALUES (:name)
 -- :name create-type! :! :n
 -- :doc creates a new type record
 INSERT INTO types
-(name, idservice)
+(typeName, idservice)
 VALUES (:name, :idservice)
 
 -- :name get-types-by-service-id :? :*
@@ -45,7 +45,7 @@ WHERE idService = :idService
 -- :name create-model! :! :n
 -- :doc creates a new type record
 INSERT INTO models
-(name)
+(modelName)
 VALUES (:name)
 
 -- :name get-models :? :*
@@ -64,29 +64,36 @@ VALUES (:name, :from_year, :to_year, :price, :description, :idModel, :idType)
 -- :doc update existing part with id
 UPDATE parts
 SET name = :name, from_year = :from_year, to_year = :to_year, price = :price, description = :description, idModel = :idModel, idType = :idType
-WHERE id = :id
+WHERE idPart = :id
 
 -- :name delete-part! :! :n
 -- :doc delete part with id
 DELETE FROM parts
-WHERE id = :id
+WHERE idPart = :id
 
 -- :name get-part-by-id :? :1
 -- :doc selects part that has this id
 SELECT * FROM parts
-WHERE id = :id
+WHERE idPart = :id
 
 -- :name get-parts :? :*
 -- :doc shows all parts
 SELECT * from parts
+    JOIN types ON parts.idType = types.idType
+    JOIN models ON parts.idModel = models.idModel
 
 -- :name get-my-parts :? :*
--- :doc shows all parts with model and year user entered
-SELECT * FROM `parts` JOIN types ON parts.idType = types.id
-                      JOIN services ON types.idService = services.id
-                      JOIN models ON parts.idModel = models.id
-                        WHERE services.id = idService AND models.id = idModel
-                            AND :year BETWEEN parts.from_year AND parts.to_year;
+-- :doc shows all parts with model and year user entered (with type parts)
+SELECT *FROM parts
+         JOIN types ON parts.idType = types.idType
+         JOIN models ON parts.idModel = models.idModel
+WHERE models.idModel = :model1
+    AND :year1 BETWEEN parts.from_year AND parts.to_year
+    AND
+        (CASE
+            WHEN :type1 != 0 THEN parts.idType = :type1
+            ELSE 1
+        END);
 
 --------------------CART-------------------------------------
 
@@ -99,7 +106,7 @@ VALUES (:idUser, :idPart)
 -- :name delete-from-cart! :! :n
 -- :doc delete cart with id
 DELETE FROM cart
-WHERE id = :id
+WHERE idCart = :id
 
 -- :name get-cart-for-user-id :? :1
 -- :doc selects cart items for user
--- src/clj/carservice/routes/home.clj
+++ src/clj/carservice/routes/home.clj
@@ -3,6 +3,7 @@
    [carservice.layout :as layout]
    [carservice.db.core :as db]
    [clojure.java.io :as io]
+   [struct.core :as st]
    [carservice.middleware :as middleware]
    [ring.util.response]
    [ring.util.http-response :as response]))
@@ -15,13 +16,28 @@
 (defn about-page [request]
   (layout/render request "about.html"))
 
-(defn bigService-page [{:keys [flash] :as request}]
-  (layout/render
-    request
-    "search/bigService.html"
-    (merge {:models (db/get-models)}
-           {:types (db/get-types-by-service-id {:idService 1})}
-    (select-keys flash [:year :errors]))))
+(defn part-page [request]
+  (let [parts (:parts request)]
+    (println "Parts data:" parts)
+    (layout/render request "parts.html" {:data {:parts parts}})))
+
+
+
+(defn bigService-page [request]
+  (let [{:keys [flash parts] :as request} request]
+    (layout/render
+      request
+      "search/bigService.html"
+      (merge {:models (db/get-models)}
+             {:types (db/get-types-by-service-id {:idService 1})}
+             ;PART THAT DOESN'T WORK
+             ; {:parts parts}
+             {:parts (db/get-my-parts {:model1 1, :type1 0, :year1 2010})}
+             (println "Parts data:" parts)
+             (select-keys flash [:model1 :type1 :year1 :errors])))))
+
+
+
 
 (defn smallService-page [request]
   (layout/render request "smallService.html"))
@@ -30,7 +46,10 @@
   (layout/render request "sensors.html"))
 
 (defn allParts-page [request]
-  (layout/render request "about.html"))
+  (layout/render
+    request
+    "parts/allParts.html"
+    {:parts (db/get-parts)}))
 
 (defn deletePart-page [request]
   (layout/render request "about.html"))
@@ -52,11 +71,60 @@
 
 ; --- SCHEMA ----------------------------------------------------------------------
 
+;(Searching for types)
+(def search-schema
+  [[:model1
+    st/required]
+   [:type1
+     st/required]
+  [:year1
+    st/required
+    {:message "ID must be positive number!"
+     :validate (fn [year] (> (Integer/parseInt (re-find #"\A-?\d+" year)) 0))}]])
+
+;(Add part to cart)
+(def idCheck-schema
+  [[:id
+    st/required]
+])
 
 ; --- VALIDATION -----------------------------------------------------------------
 
+;Search part
+(defn validate-search [params]
+  (first (st/validate params search-schema)))
+
+;IdCheck
+(defn validate-id [params]
+  (first (st/validate params idCheck-schema)))
 
 ; --- METHODE ---------------------------------------------------------------------
+
+;SEARCH MAJOR SERVICE PART
+(defn search-parts [{:keys [params]}]
+  (let [errors (validate-search params)]
+    (if (seq errors)
+      (-> (response/found "/majorService")
+          (assoc :flash (assoc params :errors errors)))
+      (let [params-no-token (dissoc params :__anti-forgery-token)
+            parts (db/get-my-parts params-no-token)]
+        (-> (response/found "/majorService")
+            (assoc :parts parts)
+            (assoc :flash (assoc params :errors {})))))))
+
+;ADD PART TO CART
+(defn add-to-cart [{:keys [params]}]
+  (let [errors (validate-id params)]
+    (if errors
+      (-> (response/found "/majorService")
+          (assoc :flash (assoc params :errors errors)))
+      (let [part (db/get-part-by-id {:id (:id params)})]
+        (if-not part
+          (-> (response/found "/majorService")
+              (assoc :flash (assoc params :errors {:id "Part with ID doesnt exist!!"})))
+          (do
+            (db/add-to-cart! {:idUser 1 :idPart (:id params)})
+            (response/found "/cart")))))))
 
 
 ; --- ROUTING --------------------------------------------------------------------------
@@ -66,7 +134,11 @@
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
 
+   ;API
+   ["/searchForBigService" {:post search-parts}]
+   ["/addToCart" {:post add-to-cart}]
 
+   ;Routes
    ["/" {:get home-page}]
    ["/about" {:get about-page}]
 
@@ -80,8 +152,8 @@
    ["/updatePart" {:get updatePart-page}]
 
 
+   ["/parts" {:get part-page}]
    ["/cart" {:get cart-page}]
-   ["/addToCart" {:get addToCart-page}]
    ["/deleteFromCart" {:get deleteFromCart-page}]
 
    ])
